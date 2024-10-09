ab860c7bbb1b9c30279e4163428d55f8337334f0
afgangalih <rhopaloceus@gmail.com>
2024-10-09 15:57:06 +0700

    Job 7 Prak1

---
+++ JS7_PHPFORM/Praktikum1(FunctionIsset)/isset.php
@@ -0,0 +1,16 @@
+<?php
+
+$umur;
+if (isset($umur) && $umur >= 18) {
+    echo "Anda Sudah Dewasa<br>";
+} else {
+    echo "Anda Belum Dewasa atau variabel 'umur' tidak ditemukan<br>";
+}
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"] . "<br>";
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array<br>";
+}
+?>
